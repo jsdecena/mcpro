f0b11a7403d8e6d288c3fc3dbb793810f494b4f6
low-blow <fadeevsn92@gmail.com>
2022-02-02 22:47:48 +0400

    Update routes

--- src/routes.php
+++ src/routes.php
@@ -2,11 +2,11 @@
 
 Route::group(['prefix' => 'api/v1' ], function () {
 
-    Route::get('country', 																	'Jsdecena\MCPro\Http\Controllers\Api\CountryApiController@index');
-    Route::get('country/{country_id}', 														'Jsdecena\MCPro\Http\Controllers\Api\CountryApiController@show');
-    Route::get('country/{country_id}/province', 											'Jsdecena\MCPro\Http\Controllers\Api\ProvincesApiController@index');
-    Route::get('country/{country_id}/province/{province_id}',								'Jsdecena\MCPro\Http\Controllers\Api\ProvincesApiController@show');
-    Route::get('country/{country_id}/province/{province_id}/city',							'Jsdecena\MCPro\Http\Controllers\Api\CityApiController@index');
-    Route::get('country/{country_id}/province/{province_id}/city/{city_id}',				'Jsdecena\MCPro\Http\Controllers\Api\CityApiController@show');
-    Route::get('country/{country_id}/province/{province_id}/city/{city_id}/barangay',		'Jsdecena\MCPro\Http\Controllers\Api\BarangayApiController@index');
+    Route::get('country', 																	'LowBlow\MCPro\Http\Controllers\Api\CountryApiController@index');
+    Route::get('country/{country_id}', 														'LowBlow\MCPro\Http\Controllers\Api\CountryApiController@show');
+    Route::get('country/{country_id}/province', 											'LowBlow\MCPro\Http\Controllers\Api\ProvincesApiController@index');
+    Route::get('country/{country_id}/province/{province_id}',								'LowBlow\MCPro\Http\Controllers\Api\ProvincesApiController@show');
+    Route::get('country/{country_id}/province/{province_id}/city',							'LowBlow\MCPro\Http\Controllers\Api\CityApiController@index');
+    Route::get('country/{country_id}/province/{province_id}/city/{city_id}',				'LowBlow\MCPro\Http\Controllers\Api\CityApiController@show');
+    Route::get('country/{country_id}/province/{province_id}/city/{city_id}/barangay',		'LowBlow\MCPro\Http\Controllers\Api\BarangayApiController@index');
 });
